6f9827f01dd8d7dc2f25aa137bd5eee44e915061
yecongling <499475142@qq.com>
2021-07-19 23:11:45 +0800

    添加异步任务管理与生产

---
+++ codeRain-framework/src/main/java/cn/ycl/framework/manager/factory/AsyncFactory.java
@@ -0,0 +1,13 @@
+package cn.ycl.framework.manager.factory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 异步工程，产生任务用
+ */
+public class AsyncFactory {
+    private static final Logger sys_user_logger = LoggerFactory.getLogger("sys-user");
+
+
+}
