6e75ee08c537c0ebd697cdf0f0ac7a4b5f22a250
yecongling <499475142@qq.com>
2021-07-28 22:20:26 +0800

    格式化

--- codeRain-common/src/main/java/cn/ycl/common/exception/user/UserPasswordRetryLimitCountException.java
+++ codeRain-common/src/main/java/cn/ycl/common/exception/user/UserPasswordRetryLimitCountException.java
@@ -8,8 +8,7 @@ package cn.ycl.common.exception.user;
 public class UserPasswordRetryLimitCountException extends UserException {
     private static final long serialVersionUID = 1L;
 
-    public UserPasswordRetryLimitCountException(int retryLimitCount)
-    {
+    public UserPasswordRetryLimitCountException(int retryLimitCount){
         super("user.password.retry.limit.count", new Object[] { retryLimitCount });
     }
 }
--- codeRain-common/src/main/java/cn/ycl/common/exception/user/UserPasswordRetryLimitExceedException.java
+++ codeRain-common/src/main/java/cn/ycl/common/exception/user/UserPasswordRetryLimitExceedException.java
@@ -8,8 +8,7 @@ package cn.ycl.common.exception.user;
 public class UserPasswordRetryLimitExceedException extends UserException {
     private static final long serialVersionUID = 1L;
 
-    public UserPasswordRetryLimitExceedException(int retryLimitCount)
-    {
+    public UserPasswordRetryLimitExceedException(int retryLimitCount){
         super("user.password.retry.limit.exceed", new Object[] { retryLimitCount });
     }
 }
