d391027ba883d7f7e121f96b14e456a239132266
yecongling <499475142@qq.com>
2021-08-17 22:46:42 +0800

    添加系统角色

--- sql/coderain.sql
+++ sql/coderain.sql
@@ -1,7 +1,9 @@
+
+/* 系统菜单表 */
 create table if not exists sys_menu
 (
-    menu_id     bigint auto_increment comment '菜单ID'
-        primary key,
+    menu_id     bigint auto_increment comment '菜单ID' primary key,
+    role_id     bigint(20)               not null comment '菜单所属角色',
     menu_name   varchar(50)              not null comment '菜单名称',
     parent_id   bigint       default 0   null comment '父菜单ID',
     order_num   int(4)       default 0   null comment '显示顺序',
@@ -17,9 +19,11 @@ create table if not exists sys_menu
 )engine=innodb default charset=utf8
     comment '菜单权限表';
 
+/* 系统用户表 */
 create table if not exists sys_user(
     user_id         bigint auto_increment comment '用户ID' primary key,
     dept_id         bigint                    null comment '部门ID',
+    role_id         bigint(20)   default '2'  not null comment '用户所属角色',
     login_name      varchar(30)               not null comment '登录账号',
     user_name       varchar(30)  default ''   null comment '用户昵称',
     user_type       varchar(2)   default '00' null comment '用户类型（00系统用户 01注册用户）',
@@ -42,6 +46,32 @@ create table if not exists sys_user(
 )  engine=innodb default charset=utf8
     comment '用户信息表';
 
+-- ----------------------------
+-- 角色信息表
+-- ----------------------------
+drop table if exists sys_role;
+create table sys_role (
+    role_id           bigint(20)      not null auto_increment    comment '角色ID',
+    role_name         varchar(30)     not null                   comment '角色名称',
+    role_key          varchar(100)    not null                   comment '角色权限字符串',
+    role_sort         int(4)          not null                   comment '显示顺序',
+    data_scope        char(1)         default '1'                comment '数据范围（1：全部数据权限 2：自定数据权限 3：本部门数据权限 4：本部门及以下数据权限）',
+    status            char(1)         not null                   comment '角色状态（0正常 1停用）',
+    del_flag          char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
+    create_by         varchar(64)     default ''                 comment '创建者',
+    create_time       datetime                                   comment '创建时间',
+    update_by         varchar(64)     default ''                 comment '更新者',
+    update_time       datetime                                   comment '更新时间',
+    remark            varchar(500)    default null               comment '备注',
+    primary key (role_id)
+) engine=innodb auto_increment=100 default charset=utf8 comment = '角色信息表';
+
+
+-- ----------------------------
+-- 初始化-角色信息表数据
+-- ----------------------------
+insert into sys_role values('1', '超级管理员', 'admin',  1, 1, '0', '0', 'admin', sysdate(), '', null, '超级管理员');
+insert into sys_role values('2', '普通角色',   'common', 2, 2, '0', '0', 'admin', sysdate(), '', null, '普通角色');
 
     -- ----------------------------
 -- 14、系统访问记录
